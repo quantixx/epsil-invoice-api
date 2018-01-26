052f670de5e2f6bf5b8a1449cb47d2ba8b0ce0d4
Antonio Goncalves <antonio.goncalves@gmail.com>
2018-01-26 14:24:54 +0100

    Getting rid of Keycloak

--- src/main/java/io/epsil/invoice/config/MicroserviceSecurityConfiguration.java
+++ src/main/java/io/epsil/invoice/config/MicroserviceSecurityConfiguration.java
@@ -1,70 +1,50 @@
 package io.epsil.invoice.config;
 
 import io.epsil.invoice.security.AuthoritiesConstants;
-import io.epsil.invoice.security.oauth2.*;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.*;
+import io.epsil.invoice.security.jwt.JWTConfigurer;
+import io.epsil.invoice.security.jwt.TokenProvider;
+
 import org.springframework.context.annotation.*;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
-
-import java.util.Map;
-import java.util.Optional;
 
 @Configuration
 @Import(SecurityProblemSupport.class)
-@EnableResourceServer
+@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-public class MicroserviceSecurityConfiguration extends ResourceServerConfigurerAdapter {
+public class MicroserviceSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private static final String OAUTH2_PRINCIPAL_ATTRIBUTE = "preferred_username";
-
-    private static final String OAUTH2_AUTHORITIES_ATTRIBUTE = "roles";
-
-    private final ResourceServerProperties resourceServerProperties;
+    private final TokenProvider tokenProvider;
 
     private final SecurityProblemSupport problemSupport;
 
-    public MicroserviceSecurityConfiguration(ResourceServerProperties resourceServerProperties,
-        SecurityProblemSupport problemSupport) {
-        this.resourceServerProperties = resourceServerProperties;
+    public MicroserviceSecurityConfiguration(TokenProvider tokenProvider, SecurityProblemSupport problemSupport) {
+        this.tokenProvider = tokenProvider;
         this.problemSupport = problemSupport;
     }
 
-    @Bean
-    @Primary
-    public UserInfoTokenServices userInfoTokenServices(PrincipalExtractor principalExtractor, AuthoritiesExtractor authoritiesExtractor) {
-        UserInfoTokenServices userInfoTokenServices =
-            new CachedUserInfoTokenServices(resourceServerProperties.getUserInfoUri(), resourceServerProperties.getClientId());
-
-        userInfoTokenServices.setPrincipalExtractor(principalExtractor);
-        userInfoTokenServices.setAuthoritiesExtractor(authoritiesExtractor);
-        return userInfoTokenServices;
-    }
-
-    @Bean
-    public PrincipalExtractor principalExtractor() {
-        return new SimplePrincipalExtractor(OAUTH2_PRINCIPAL_ATTRIBUTE);
-    }
-
-    @Bean
-    public AuthoritiesExtractor authoritiesExtractor() {
-        return new SimpleAuthoritiesExtractor(OAUTH2_AUTHORITIES_ATTRIBUTE);
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+            .antMatchers(HttpMethod.OPTIONS, "/**")
+            .antMatchers("/app/**/*.{js,html}")
+            .antMatchers("/bower_components/**")
+            .antMatchers("/i18n/**")
+            .antMatchers("/content/**")
+            .antMatchers("/swagger-ui/index.html")
+            .antMatchers("/test/**")
+            .antMatchers("/h2-console/**");
     }
 
     @Override
-    public void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
         http
             .csrf()
             .disable()
@@ -80,38 +60,20 @@ public class MicroserviceSecurityConfiguration extends ResourceServerConfigurerA
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
             .authorizeRequests()
-            .antMatchers("/api/profile-info").permitAll()
             .antMatchers("/api/**").authenticated()
             .antMatchers("/management/health").permitAll()
-            .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN);
+            .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
+            .antMatchers("/swagger-resources/configuration/ui").permitAll()
+        .and()
+            .apply(securityConfigurerAdapter());
+    }
+
+    private JWTConfigurer securityConfigurerAdapter() {
+        return new JWTConfigurer(tokenProvider);
     }
 
     @Bean
-    @ConditionalOnProperty("security.oauth2.resource.jwt.key-uri")
-    public TokenStore tokenStore(JwtAccessTokenConverter jwtAccessTokenConverter) {
-        return new JwtTokenStore(jwtAccessTokenConverter);
-    }
-
-    @Bean
-    @ConditionalOnProperty("security.oauth2.resource.jwt.key-uri")
-    public JwtAccessTokenConverter jwtAccessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setVerifierKey(getKeyFromAuthorizationServer());
-        return converter;
-    }
-
-    private String getKeyFromAuthorizationServer() {
-        return Optional.ofNullable(
-            new RestTemplate()
-                .exchange(
-                    resourceServerProperties.getJwt().getKeyUri(),
-                    HttpMethod.GET,
-                    new HttpEntity<Void>(new HttpHeaders()),
-                    Map.class
-                )
-                .getBody()
-                .get("public_key"))
-            .map(publicKey -> String.format("-----BEGIN PUBLIC KEY-----\n%s\n-----END PUBLIC KEY-----", publicKey))
-            .orElse(resourceServerProperties.getJwt().getKeyValue());
+    public SecurityEvaluationContextExtension securityEvaluationContextExtension() {
+        return new SecurityEvaluationContextExtension();
     }
 }
